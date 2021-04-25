009f4c83732f0852bebaf79d8920bd5a98f992be
h587730 <54102651+h587730@users.noreply.github.com>
2021-04-25 23:11:12 +0200

    Delete boatshr directory

--- boatshr/src/main/java/com/example/demo/dao/BPRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.model.Baatparkering;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BPRepo extends CrudRepository<Baatparkering, Integer> {
-
-
-}
--- boatshr/src/main/java/com/example/demo/dao/SelskapRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.model.Selskap;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SelskapRepo extends CrudRepository<Selskap, Integer> {
-
-}
